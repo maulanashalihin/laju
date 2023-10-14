1255544a41ec7c24ca0369ab2a793bbea3070f38
maulana <maulana@drip.id>
2023-10-14 21:31:28 +0800

    simple route

--- routes/web.ts
+++ routes/web.ts
@@ -14,18 +14,15 @@ Route.get("/register", AuthController.registerPage);
 Route.post("/register", AuthController.register, { body: "json" });
 Route.post("/logout", AuthController.logout);
 
-Route.guard("/auth", async (ctx : any) => {
+Route.guard("/auth", async (ctx: any) => {
    const session_id = Cookie(ctx).get("session_id");
 
    if (session_id) {
       let session_ = await Session.get(session_id);
 
-      
       if (!session_) {
          return null;
       }
-
-      
 
       return true;
    }
