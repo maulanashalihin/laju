fb63598cc02f8b02df240265dd1b48678eea5240
maulana <maulana@drip.id>
2023-10-15 18:46:15 +0800

    make index more clean

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import Asset from './app/responses/Asset';
+
 import Route from './routes/web'; 
 
-
 Route.get("/asset/*",Asset) 
-
 
 Route.get("/favicon.ico",()=>new Response(Bun.file("public/favicon/favicon.ico")))
 
