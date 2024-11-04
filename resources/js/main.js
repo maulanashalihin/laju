f629059a76b4287bdb7c2171a770e0d52fd4bc75
maulana <maulana@drip.id>
2024-11-04 14:28:22 +0800

    homepage setup

--- resources/js/main.js
+++
@@ -1,9 +0,0 @@
-import * as pages from "./Pages/**/*"
-import { createInertiaApp } from '@inertiajs/svelte' 
-
-createInertiaApp({
-    resolve: name => pages[name.replace("/", "")],
-    setup({ el, App, props }) {
-      new App({ target: el, props })
-    },
-  })
