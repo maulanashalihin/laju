8c5bc57a3db2bd77b44e41c4102a8135d9392601
Maulana Shalihin <45333365+maulanashalihin@users.noreply.github.com>
2025-02-16 20:17:23 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import 'dotenv/config'
+import { resolve } from 'path'
 import { readdirSync } from 'fs';
 
 const files = readdirSync("resources/views");
@@ -8,9 +9,9 @@ const files = readdirSync("resources/views");
 let input = {};
 
 for (const filename of files) {
-  input[filename.replace(".html", "")] = `resources/views/${filename}`;
+  input[filename.replace(".html", "")] = resolve(__dirname, `resources/views/${filename}`);
 }
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
@@ -22,10 +23,9 @@ export default defineConfig({
   
   build: {
     outDir: '../dist',
+    emptyOutDir: true,
     rollupOptions: {
-      rollupOptions: {
-        input
-      }
+      input:  input
     }
   }
 })
